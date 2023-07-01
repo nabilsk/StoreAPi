9f58064e533461ae186e19a5b93796f6a6be7f79
nabilsk <nabilshaikh7730@gmail.com>
2023-07-02 00:52:48 +0530

    added some filter searching

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,29 @@
-const getAllProductsStatic = (req, res) => {
-  res.status(200).json({ msg: "products testing route" });
+const Product = require("../models/product");
+
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({}).sort("price");
+  res.status(200).json({ products, nbHits: products.length });
 };
 
-const getAllProducts = (req, res) => {
-  res.status(200).json({ msg: "products route" });
+const getAllProducts = async (req, res) => {
+  const { featured, company, name } = req.query;
+  const queryObj = {};
+
+  if (featured) {
+    queryObj.featured = featured === "true" ? true : false;
+  }
+
+  if (company) {
+    queryObj.company = company;
+  }
+
+  if (name) {
+    queryObj.name = { $regex: name, $options: "i" };
+  }
+
+  console.log("test: ", queryObj);
+  const products = await Product.find(queryObj);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
